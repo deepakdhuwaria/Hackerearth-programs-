e99db527592a45adcc16fdcb360bedeb70e8dfab
deepakdhuwaria <43870379+deepakdhuwaria@users.noreply.github.com>
2019-10-26 21:28:13 +0530

    Added Comments

--- Friends's Relationship.cpp
+++ Friends's Relationship.cpp
@@ -3,16 +3,16 @@ using namespace std;
 int main()
 {
     int t;
-    cin>>t;
+    cin>>t;   //Taking input for number of test cases
     while(t--)
     {
-        int n,star,hash;
-        cin>>n;
+        int n,star,hash;  
+        cin>>n;   //Taking input for number of Rows
         
-        int c=2*(n-1)+2;
+        int c=2*(n-1)+2;  //Calculating Number of Columns in the pattern
         for(int i=1;i<=n;i++)
         {
-            hash=2*(n-1)-((i-1)*2);
+            hash=2*(n-1)-((i-1)*2); //Calculating number of hash in the current row
             star=i;
             for(int j=1;j<=c;j++)
             {
